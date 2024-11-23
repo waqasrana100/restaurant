b7edf2579aa0db6f87c8f6728ba3a61499444c09
waqasCodeNinja <rana.waqas@codeninjaconsulting.com>
2024-11-24 00:24:05 +0500

    cjage

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,17 +1,17 @@
-"use client";
-
 import '../globals.css';
-import { Inter } from 'next/font/google'
-import { notFound } from 'next/navigation';
-import { NextIntlClientProvider, useMessages } from 'next-intl';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
 import { ReactNode } from 'react';
+import { NextIntlClientProvider } from 'next-intl';
+import { notFound } from 'next/navigation';
 import { locales } from '@/config';
-import { ThemeProvider } from '@/components/theme-provider'
-import Navbar from '@/components/navbar'
-import Footer from '@/components/footer'
+import { ThemeProvider } from '@/components/theme-provider';
+import Navbar from '@/components/navbar';
+import Footer from '@/components/footer';
 import { Toaster } from 'sonner';
+import { getTranslations, unstable_setRequestLocale } from 'next-intl/server';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 type Props = {
   children: ReactNode;
@@ -22,10 +22,28 @@ export function generateStaticParams() {
   return locales.map((locale) => ({ locale }));
 }
 
-export default function LocaleLayout({ children, params: { locale } }: Props) {
-  const messages = useMessages();
+export async function generateMetadata({ params: { locale } }: Props): Promise<Metadata> {
+  const t = await getTranslations({ locale, namespace: 'Metadata' });
 
+  return {
+    title: t('title'),
+    description: t('description'),
+  };
+}
+
+export default async function LocaleLayout({ children, params: { locale } }: Props) {
+  // Enable static rendering
+  unstable_setRequestLocale(locale);
+
+  // Validate that the incoming `locale` parameter is valid
   if (!locales.includes(locale as any)) notFound();
+
+  let messages;
+  try {
+    messages = (await import(`@/messages/${locale}.json`)).default;
+  } catch (error) {
+    notFound();
+  }
 
   return (
     <html lang={locale}>
